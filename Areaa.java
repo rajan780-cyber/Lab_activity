5f8b8af710f5cfe6c4dd90aed622b84eb8ec44bb
rajan780-cyber <53599652+rajan780-cyber@users.noreply.github.com>
2022-08-03 10:25:45 +0530

    Add files via upload

---
+++ Areaa.java
@@ -0,0 +1,12 @@
+public class Areaa
+{
+ public static void main(String[] args)
+ {
+  int a=18,b=6,area;
+  area=a*b;
+  System.out.println("area of rectangle is"+area);
+  int side=12,area_of_square;
+  area_of_square=side*side;
+  System.out.println("area of square is"+area);
+  }
+ }
